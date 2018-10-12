2f6cd6bcb84675eb327c7a7f023ffa833089c754
Lukas Wolfsteiner <lukas@wolfsteiner.de>
2018-10-12 22:35:36 +0200

    Initial Commit

---
+++ Shortcuts for Pi-hole/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Shortcuts for Pi-hole
+//
+//  Created by Lukas Wolfsteiner on 12.10.18.
+//  Copyright © 2018 Lukas Wolfsteiner. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+

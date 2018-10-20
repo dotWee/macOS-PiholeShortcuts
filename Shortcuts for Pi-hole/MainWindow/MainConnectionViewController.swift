e3ab64aae1dc1102d6b34e7628db5b009fd0f47e
Lukas Wolfsteiner <lukas@wolfsteiner.de>
2018-10-20 22:03:56 +0200

    Implement bottom status bar to display connection details

--- Shortcuts for Pi-hole/MainWindow/MainConnectionViewController.swift
+++ Shortcuts for Pi-hole/MainWindow/MainConnectionViewController.swift
@@ -9,21 +9,34 @@
 import Cocoa
 
 class MainConnectionViewController: NSViewController {
+    var connectionStatus: ConnectionStatus?
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         // Do view setup here.
+        self.displayBuiltUrl()
     }
     
-    @IBAction func connectionButtonActionHandler(_ sender: Any) {
-        let configStatus = PiHoleProxy.getConfigStatus()
+    @IBOutlet weak var coloredStatusViewOutlet: ColoredStatusView!
+    
+    @IBOutlet weak var statusMessageLabelOutlet: NSTextField!
+    
+    func displayBuiltUrl() {
+        connectionStatus = PiHoleProxy.getConfigStatus()
         
         // config valid
-        if configStatus.color == NSColor.green {
+        if connectionStatus!.color == NSColor.green {
             // display url
             let url = PiHoleProxy.getBaseUrl(action: PiHoleAction.Status)
             exampleRequestUrlTextField.stringValue = url!.absoluteString
+            
+            statusMessageLabelOutlet.stringValue = "Press connect to verify"
+            coloredStatusViewOutlet.updateFillingColor(color: NSColor.gray)
         } else {
+            statusMessageLabelOutlet.stringValue = "Invalid configuration"
+            coloredStatusViewOutlet.updateFillingColor(color: NSColor.red)
+            
             // show invalid config alert
             let alert = NSAlert()
             alert.messageText = "Invalid configuration"
@@ -35,6 +48,28 @@ class MainConnectionViewController: NSViewController {
         }
     }
     
+    @IBAction func connectionButtonActionHandler(_ sender: Any) {
+        self.displayBuiltUrl()
+        
+        if connectionStatus!.color == NSColor.green {
+            statusMessageLabelOutlet.stringValue = "Requesting..."
+            coloredStatusViewOutlet.updateFillingColor(color: NSColor.yellow)
+            
+            PiHoleProxy.performActionRequest(PiHoleAction.Status, onSuccess: { (status) in
+                DispatchQueue.main.async {
+                    self.testConnectionTextView.string = "Current Pi-hole status: " + status
+                    self.statusMessageLabelOutlet.stringValue = "Connection established"
+                    self.coloredStatusViewOutlet.updateFillingColor(color: NSColor.green)
+                }
+            }) { (error) in
+                DispatchQueue.main.async {
+                    self.testConnectionTextView.string = error.debugDescription
+                    self.statusMessageLabelOutlet.stringValue = "An error occurred"
+                    self.coloredStatusViewOutlet.updateFillingColor(color: NSColor.red)
+                }
+            }
+        }
+    }
     
     @IBOutlet weak var exampleRequestUrlTextField: NSTextField!
 

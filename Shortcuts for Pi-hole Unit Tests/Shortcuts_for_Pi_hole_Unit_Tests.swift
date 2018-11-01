85d761a59ce2ccfba9ac0c0210558539254bcc18
Lukas Wolfsteiner <lukas@wolfsteiner.de>
2018-11-01 13:10:08 +0100

    Add default unit testing bundle to project

---
+++ Shortcuts for Pi-hole Unit Tests/Shortcuts_for_Pi_hole_Unit_Tests.swift
@@ -0,0 +1,33 @@
+//
+//  Shortcuts_for_Pi_hole_Unit_Tests.swift
+//  Shortcuts for Pi-hole Unit Tests
+//
+//  Created by Lukas Wolfsteiner on 01.11.18.
+//  Copyright © 2018 Lukas Wolfsteiner. All rights reserved.
+//
+
+import XCTest
+
+class Shortcuts_for_Pi_hole_Unit_Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
